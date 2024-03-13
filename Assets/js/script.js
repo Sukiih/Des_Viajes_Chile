29c0656059831416f3f1b910fafc9f6f2f17baf2
Waleska Silva <vale.sgonzalez2011@hotmail.com>
2024-03-12 23:25:07 -0300

    ADD: footer

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,7 +3,7 @@ window.addEventListener('scroll', function() {
     console.log("Desplazamiento activado"); 
     var navbar = document.getElementById('navbar');
     if (window.scrollY > 10) {
-        navbar.style.backgroundColor = '#16a2b7';
+        navbar.style.backgroundColor = 'black';
     } else {
         navbar.style.backgroundColor = 'transparent';
     }
@@ -33,4 +33,19 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
     var myCarousel = document.getElementById('inicio');
     var carousel = new bootstrap.Carousel(myCarousel);
+});
+
+//Alerta de correo enviado formulario
+$('#envia_correo').click(function () {
+    alert("El correo fue enviado correctamente...");
+});
+
+//función de tooltip en formulario
+$(document).ready(function () {
+    $("#envia_correo").tooltip();
+});
+
+//tooltip barra footer
+$(document).ready(function() {
+    $('[data-bs-toggle="tooltip"]').tooltip();
 });
